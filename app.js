98cbc90f121be0360e6f27617d3eceffb26ad5cd
HarunPHero <harun7309@gmail.com>
2021-09-13 13:19:44 +0600

    search your song

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 
 const searchSongs = () => {
     const searchText = document.getElementById('search-field').value;
-    const url = `https://api.lyrics.ovh/suggest/:${searchText}`
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
     // load data
     fetch(url)
         .then(res => res.json())
         .then(data => displaysongs(data.data))
-        .catch(error => errormass('404 not found'))
+        .catch(error => errormass("Sorry, song not found"))
 
 }
+const errormass = error => {
+    const errortag = document.getElementById('error-message');
+    errortag.innerText = error
+};
+
 const displaysongs = songs => {
     const songContainer = document.getElementById('song-container')
     songContainer.innerHTML = "";
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = ""
     songs.forEach(song => {
         const songsDiv = document.createElement('div')
         songsDiv.className = "single-result row align-items-center my-3 p-3"
@@ -20,9 +27,9 @@ const displaysongs = songs => {
                 <h3 class="lyrics-name">${song.title}</h3>
                 <p class="author lead">Album by <span>${song.artist.name}</span></p>
                 <audio controls>
-  <source src="${song.preview}" type="">
-  
-</audio>
+                     <source src="${song.preview}" type="audio/mpeg">
+                </audio>
+
         </div>
         <div class="col-md-3 text-md-right text-center">
                 <img src="${song.album.cover}">
@@ -33,6 +40,7 @@ const displaysongs = songs => {
     });
 }
 const getLyric = (artist, title) => {
+    
     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
     fetch(url)
         .then(res => res.json())
@@ -41,11 +49,9 @@ const getLyric = (artist, title) => {
 }
 const displayLyrics = lyrics => {
     const lyricsDiv = document.getElementById('song-lyrics');
+    
     lyricsDiv.innerText = lyrics
+    
 }
-const errormass = error => {
-    const errortag = document.getElementById('error-message');
-    errortag.innerText = error
-};
 
 
