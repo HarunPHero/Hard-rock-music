b7c7259ac476a59016bb1be1c496b44afe6fddfb
HarunPHero <harun7309@gmail.com>
2022-03-03 19:03:08 +0600

    hard-rock

--- app.js
+++ app.js
@@ -1,64 +1,57 @@
 const input = document.getElementById("search-field");
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("enter").click();
+    event.preventDefault();
+    document.getElementById("enter").click();
   }
 });
-const searchSongs = () => {
-    const searchText = document.getElementById('search-field').value;
-    const url = `https://api.lyrics.ovh/suggest/${searchText}`
+const searchBtn = document.getElementById("enter").addEventListener("click", function () {
+    const searchText = document.getElementById("search-field").value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`;
     // load data
     fetch(url)
-        .then(res => res.json())
-        .then(data => displaysongs(data.data))
-        .catch(error => errormass("Sorry, song not found"))
+      .then((res) => res.json())
+      .then((data) => displaysongs(data.data))
+      .catch((error) => errormass("Sorry, song not found"));
+  });
 
-}
-const errormass = error => {
-    const errortag = document.getElementById('error-message');
-    errortag.innerText = error
+const errormass = (error) => {
+  const errortag = document.getElementById("error-message");
+  errortag.innerText = error;
 };
 
-const displaysongs = songs => {
-    const songContainer = document.getElementById('song-container')
-    songContainer.innerHTML = "";
-    const lyricsDiv = document.getElementById('song-lyrics');
-    lyricsDiv.innerText = ""
-    songs.forEach(song => {
-        const songsDiv = document.createElement('div')
-        songsDiv.className = "single-result row align-items-center my-3 p-3"
-        songsDiv.innerHTML = `
+const displaysongs = (songs) => {
+  const songContainer = document.getElementById("song-container");
+  songContainer.innerHTML = "";
+  const lyricsDiv = document.getElementById("song-lyrics");
+  lyricsDiv.innerText = "";
+  songs.forEach((song) => {
+    const songsDiv = document.createElement("div");
+    songsDiv.className = "single-result row align-items-center my-3 p-3";
+    songsDiv.innerHTML = `
         <div class="col-md-9">
                 <h3 class="lyrics-name">${song.title}</h3>
                 <p class="author lead">Album by <span>${song.artist.name}</span></p>
-                <audio controls>
-                     <source src="${song.preview}" type="audio/ogg">
-                     <source src="${song.preview}" type="audio/mpeg">
-                </audio>
+                <a class="btn btn-success" href="${song.preview}">Listen the song</a>
+                <a href="${song.preview}" download>Lsten the song</a>
 
         </div>
         <div class="col-md-3 text-md-right text-center">
                 <img src="${song.album.cover}">
-                <button onclick="getLyric('${song.artist.name}', '${song.title}')"class="btn btn-success">Get Lyrics</button>
+                
         </div>`;
 
-        songContainer.appendChild(songsDiv)
-    });
-}
-const getLyric = (artist, title) => {
-    
-    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayLyrics(data.lyrics))
-        
-}
-const displayLyrics = lyrics => {
-    const lyricsDiv = document.getElementById('song-lyrics');
-    
-    lyricsDiv.innerText = lyrics
-    
-}
+    songContainer.appendChild(songsDiv);
+  });
+};
+// const getLyric = (artist, title) => {
+//   const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => displayLyrics(data.lyrics));
+// };
+// const displayLyrics = (lyrics) => {
+//   const lyricsDiv = document.getElementById("song-lyrics");
 
-
+//   lyricsDiv.innerText = lyrics;
+// };
